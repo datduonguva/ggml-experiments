1bd81f0af9c5cdca3f7f66e7ceefd1734502c04d
dat duong <dqd8tp@virginia.edu>
2024-02-25 10:16:10 -0500

    more readind, need reading of moving mean and variance

--- mobilevit/main.cpp
+++ mobilevit/main.cpp
@@ -108,6 +108,9 @@ struct mobile_vit_layer {
     mobilevit_conv_layer  conv_kxk;
     mobilevit_conv_layer conv_1x1;
     mobilevit_transformer transformer;
+    
+    ggml_tensor * layernorm_alpha;
+    ggml_tensor * layernorm_beta;
     mobilevit_conv_layer conv_projection;
     mobilevit_conv_layer fusion;
 };
@@ -170,6 +173,7 @@ void read_weights(ggml_tensor * tensor, ggml_context * ctx_w, std::ifstream &fin
     );
 }
  
+// overloading read mobileit_conv_layer
 void read_weights(
     mobilevit_conv_layer & layer,
     ggml_context * ctx_w,
@@ -180,6 +184,75 @@ void read_weights(
     read_weights(layer.kernel, ctx_w, fin); 
     if (read_gamma) read_weights(layer.gamma, ctx_w, fin);
     if (read_beta) read_weights(layer.beta, ctx_w, fin);
+}
+
+// overloading read transformer layers
+void read_weights(
+    mobilevit_transformer_layer & layer,
+    ggml_context * ctx_w,
+    std::ifstream &fin,
+    bool read_gamma=true,
+    bool read_beta=true
+){
+
+    read_weights(layer.attention_query_kernel, ctx_w, fin);
+    read_weights(layer.attention_query_bias, ctx_w, fin);
+
+    read_weights(layer.attention_key_kernel, ctx_w, fin); 
+    read_weights(layer.attention_key_bias, ctx_w, fin);
+
+    read_weights(layer.attention_value_kernel, ctx_w, fin);
+    read_weights(layer.attention_value_bias, ctx_w, fin);
+
+    read_weights(layer.attention_output_kernel, ctx_w, fin);
+    read_weights(layer.attention_output_bias, ctx_w, fin);
+
+
+    // intermediate
+    read_weights(layer.intermediate_kernel, ctx_w, fin);
+    read_weights(layer.intermediate_kernel, ctx_w, fin);   
+
+
+    // output
+    read_weights(layer.output_kernel, ctx_w, fin);
+    read_weights(layer.output_bias, ctx_w, fin);
+
+    // layernorm_before
+    read_weights(layer.lb_gamma, ctx_w, fin);
+    read_weights(layer.lb_beta, ctx_w, fin);
+
+    // layernorm_after
+    read_weights(layer.la_gamma, ctx_w, fin);
+    read_weights(layer.la_beta, ctx_w, fin);
+}
+
+// overloading the read_weights ofthe mobile_vit_layer
+void read_weights(mobile_vit_layer &layer, ggml_context * ctx_w, std::ifstream &fin){
+    auto downsampling_layer = layer.downsampling_layer;
+    read_weights(downsampling_layer.expand_1x1, ctx_w, fin);
+    read_weights(downsampling_layer.conv_3x3, ctx_w, fin);
+    read_weights(downsampling_layer.reduce_1x1, ctx_w, fin);
+   
+    read_weights(layer.conv_kxk, ctx_w, fin);
+    // this conv_1x1 doesn't have normalization, so, there is no gamma and betta parameters
+    read_weights(layer.conv_1x1, ctx_w, fin, false, false);
+
+    // read the transformer layers:
+    layer.transformer.layers.resize(layer.num_stages);
+    for (int i = 0; i < layer.num_stages; i++){
+        auto transformer_layer = layer.transformer.layers[0]; 
+        read_weights(transformer_layer, ctx_w, fin);
+    }
+    // read the layernorm
+    read_weights(layer.layernorm_alpha, ctx_w, fin);
+    read_weights(layer.layernorm_beta, ctx_w, fin);
+
+    // read the projection
+    read_weights(layer.conv_projection, ctx_w, fin);
+
+    // read the fusion
+    read_weights(layer.fusion, ctx_w, fin);
+
 }
 
 
@@ -262,68 +335,52 @@ void load_model(mobilevit_model & model, std::string model_path){
             int hidden_size = model.hparams.hidden_sizes[0];
 
             model.encoder.layer_3.in_channels = in_channels;
-            
             model.encoder.layer_3.out_channels = out_channels;
             model.encoder.layer_3.num_stages = num_stages;
             model.encoder.layer_3.strides = strides;
             model.encoder.layer_3.hidden_size = hidden_size;
             model.encoder.layer_3.dilation = 1;
 
-
-            auto downsampling_layer = model.encoder.layer_3.downsampling_layer;
-            read_weights(downsampling_layer.expand_1x1, model.ctx_w, fin);
-            read_weights(downsampling_layer.conv_3x3, model.ctx_w, fin);
-            read_weights(downsampling_layer.reduce_1x1, model.ctx_w, fin);
-           
-            read_weights(model.encoder.layer_3.conv_kxk, model.ctx_w, fin);
-            // this conv_1x1 doesn't have normalization, so, there is no gamma and betta parameters
-            read_weights(model.encoder.layer_3.conv_1x1, model.ctx_w, fin, false, false);
-
-            // read the transformer layers:
-            model.encoder.layer_3.transformer.layers.resize(num_stages);
-            for (int i = 0; i < num_stages; i++){
-                auto layer = model.encoder.layer_3.transformer.layers[0]; 
-
-                read_weights(layer.attention_query_kernel, model.ctx_w, fin);
-                read_weights(layer.attention_query_bias, model.ctx_w, fin);
-                read_weights(layer.attention_key_kernel, model.ctx_w, fin); 
-                read_weights(layer.attention_key_bias, model.ctx_w, fin);
-                read_weights(layer.attention_value_kernel, model.ctx_w, fin);
-                read_weights(layer.attention_value_bias, model.ctx_w, fin);
-                read_weights(layer.attention_output_kernel, model.ctx_w, fin);
-                read_weights(layer.attention_output_bias, model.ctx_w, fin);
-
-
-                // intermediate
-                read_weights(layer.intermediate_kernel, model.ctx_w, fin);
-                read_weights(layer.intermediate_kernel, model.ctx_w, fin);   
-
-
-                // output
-                read_weights(layer.output_kernel, model.ctx_w, fin);
-                read_weights(layer.output_bias, model.ctx_w, fin);
-
-                // layernorm_before
-                read_weights(layer.lb_gamma, model.ctx_w, fin);
-                read_weights(layer.lb_beta, model.ctx_w, fin);
-
-                // layernorm_after
-                read_weights(layer.la_gamma, model.ctx_w, fin);
-                read_weights(layer.la_beta, model.ctx_w, fin);
-
-            }
+            read_weights(model.encoder.layer_3, model.ctx_w, fin);
         }
 
         // read layer 4
         {
+            int in_channels = model.hparams.neck_hidden_sizes[3];
+            int out_channels = model.hparams.neck_hidden_sizes[4];
+            int strides = 2;
+            int num_stages = 4;
+            int hidden_size = model.hparams.hidden_sizes[1];
+
+            model.encoder.layer_4.in_channels = in_channels;
             
+            model.encoder.layer_4.out_channels = out_channels;
+            model.encoder.layer_4.num_stages = num_stages;
+            model.encoder.layer_4.strides = strides;
+            model.encoder.layer_4.hidden_size = hidden_size;
+            model.encoder.layer_4.dilation = 1;
+
+            read_weights(model.encoder.layer_4, model.ctx_w, fin); 
         }
 
         // read layer 5
         {
-            
-        }
+            int in_channels = model.hparams.neck_hidden_sizes[4];
+            int out_channels = model.hparams.neck_hidden_sizes[5];
+            int strides = 2 ;
+            int num_stages = 3;
+            int hidden_size = model.hparams.hidden_sizes[2];
 
+            model.encoder.layer_4.in_channels = in_channels;
+            
+            model.encoder.layer_4.out_channels = out_channels;
+            model.encoder.layer_4.num_stages = num_stages;
+            model.encoder.layer_4.strides = strides;
+            model.encoder.layer_4.hidden_size = hidden_size;
+            model.encoder.layer_4.dilation = 1;
+
+            read_weights(model.encoder.layer_4, model.ctx_w, fin); 
+        }
     }
 
     fin.close();
